c38f4fbbc586fdc3dcf5ff4b3afcb39d810b9bc0
abbiyanaila <abbiyanaila@gmail.com>
2020-11-09 09:36:15 +0800

    feat: add response xpath

---
+++ quotes_spider/quotes_spider/spiders/quote.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class QuoteSpider(scrapy.Spider):
+    name = 'quote'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        pass
