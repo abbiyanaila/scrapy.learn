2133e852c521d777d91e554d597df052d0971111
abbiyanaila <abbiyanaila@gmail.com>
2020-11-19 20:54:43 +0800

    feat: add author and tags

--- quotes_spider/quotes_spider/spiders/quote.py
+++ quotes_spider/quotes_spider/spiders/quote.py
@@ -7,9 +7,15 @@ class QuoteSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        h1_tag = response.xpath('//h1/a/text()').extract_first()
-        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        # h1_tag = response.xpath('//h1/a/text()').extract_first()
+        # tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
 
-        yield {'H1_Tag': h1_tag, 'Tags': tags}
+        # yield {'H1_Tag': h1_tag, 'Tags': tags}
+        quotes = response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+            text = quote.xpath('.//*[@class="text"]/text()').extract_first()
+            author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
+
+            ... 
 
 
