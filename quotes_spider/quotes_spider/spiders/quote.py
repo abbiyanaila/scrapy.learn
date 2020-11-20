125992ba17e8792c7691791746131bd37aad50b1
abbiyanaila <abbiyanaila@gmail.com>
2020-11-20 17:16:37 +0800

    add: author and tags

--- quotes_spider/quotes_spider/spiders/quote.py
+++ quotes_spider/quotes_spider/spiders/quote.py
@@ -2,20 +2,26 @@ import scrapy
 
 
 class QuoteSpider(scrapy.Spider):
-    name = 'quote'
+    name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = (
+        'http://quotes.toscrape.com/')
 
     def parse(self, response):
         # h1_tag = response.xpath('//h1/a/text()').extract_first()
         # tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
-
         # yield {'H1_Tag': h1_tag, 'Tags': tags}
         quotes = response.xpath('//*[@class="quote"]')
         for quote in quotes:
             text = quote.xpath('.//*[@class="text"]/text()').extract_first()
             author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
+            tags =  quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
 
-            ... 
+            print '\n'
+            print text
+            print author
+            print tags
+            print '\n'
+    
 
 
