38360df307b0d5f00360cc4682adc1b051f6066d
abbiyanaila <abbiyanaila@gmail.com>
2020-11-30 12:01:59 +0800

    feat: add yield

--- quotes_spider/quotes_spider/spiders/quote.py
+++ quotes_spider/quotes_spider/spiders/quote.py
@@ -4,7 +4,6 @@ import scrapy
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ["quotes.toscrape.com"]
-
     start_urls = (
         'https://quotes.toscrape.com/',
     )
@@ -19,11 +18,14 @@ class QuoteSpider(scrapy.Spider):
             author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
             tags =  quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
 
-            # print '\n'
-            # print text
-            # print author
-            # print tags
-            # print '\n'
+            yield{'Text': text,
+                'Author': author,
+                'Tags': tags}
+                    
+        
+        next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
+        absolute_next_page_url = response.urljoint(next_page_url,href.extract())
+        yield scrapy.Request(absolute_next_page_url)
     
 
 
